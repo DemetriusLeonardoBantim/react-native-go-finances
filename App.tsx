460df18af8f011331c34cd25ab0c4619cfd1c086
dl <DemetriusLeonardoBantim>
2022-09-08 08:33:41 -0300

    refactor: modificar raiz do projeto para tsx

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import {ThemeProvider} from 'styled-components'
+import {Dashboard} from './src/Screens/Dashboard'
+import theme from './src/Global/stytles/theme'
+import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular, 
+    Poppins_500Medium, 
+    Poppins_700Bold
+  });
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
+}
