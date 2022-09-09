ce19417c4fda5c8782ccabbd1c82090b44ea68d5
dl <DemetriusLeonardoBantim>
2022-09-09 13:22:20 -0300

    feat: criar screen para registrar despesas

--- src/Screens/Register/index.tsx
+++ src/Screens/Register/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import {Container, Header, Title, Form, Fields} from './style'
+import React, {useState} from 'react'
+import {Container, Header, Title, Form, Fields, TransactionsType} from './style'
 
 import {Input} from '../../Components/Forms/Input'
 import {Button} from '../../Components/Forms/Button'
-
+import { TransactionTypeButton } from '../../Components/Forms/TransactionTypeButton'
+import {CategorySelect} from '../../Components/Forms/CategorySelect'
 
 export function Register(){
+  const [transactionType, setTransactionType] = useState('')
+
+  function handleTransactionTypeSelect(type: 'up' | 'down'){
+    setTransactionType(type)
+  }
+
   return(
     <Container>
       <Header>
@@ -18,6 +25,26 @@ export function Register(){
         <Fields>
           <Input placeholder='Nome'/>
           <Input placeholder='Preço'/>
+
+
+          <TransactionsType>
+            <TransactionTypeButton 
+              type='up' 
+              title='Income' 
+              onPress={() => handleTransactionTypeSelect('up')} 
+              isActive={transactionType === 'up'} 
+            />
+
+            <TransactionTypeButton 
+              type='down' 
+              title='Income' 
+              onPress={() => handleTransactionTypeSelect('down')} 
+              isActive={transactionType === 'down'} 
+            />
+            
+          </TransactionsType>
+
+          <CategorySelect title="Categoria" />
         </Fields>
 
       
