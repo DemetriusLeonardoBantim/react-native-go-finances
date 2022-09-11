5efcef09cf0e3b9b8443663fb41da5c40012c0d3
dl <DemetriusLeonardoBantim>
2022-09-11 09:50:03 -0300

    feat: importar o AppRoutes na raiz do projeto

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import {Register} from './src/Screens/Register'
 import {CategorySelect} from './src/Screens/CategorySelect'
 import theme from './src/Global/stytles/theme'
 import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins'
+import {AppRoutes} from './src/routes/app.routes'
+import {NavigationContainer} from '@react-navigation/native'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -15,7 +17,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes/>
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
