da664ddae000ce55227b9dda70cc2b1ccdfe6c91
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2022-10-10 22:42:22 -0300

    feat: screen da tela de login

---
+++ src/Screens/SignIn/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Text} from 'react-native'
+
+import { Container,Header,TitleWrapper,Title,SignInTitle,Footer } from './style'
+
+import AppSvg from '../../assets/apple.svg'
+import GoogleSvg from '../../assets/google.svg'
+import LogoSvg from '../../assets/logo.svg'
+import { RFValue } from 'react-native-responsive-fontsize'
+
+
+export function SignIn(){
+  return (
+    <Container>
+      <Header>
+        <TitleWrapper>
+          <LogoSvg width={RFValue(200)}/>
+          <Title>
+            Controle suas {'\n'} finanças de forma {'\n'} muito simples
+          </Title>
+        </TitleWrapper>
+
+        <SignInTitle>
+          Faça seu login com {'\n'} uma das contas abaixo
+        </SignInTitle>
+      </Header>
+
+      <Footer>
+
+      </Footer>
+    </Container>
+  )
+}
