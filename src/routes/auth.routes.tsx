1bcf28636eb049b6dfe1153b62b4c5c035d041ad
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2023-08-06 16:38:38 +0000

    remove header

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { useTheme } from 'styled-components';
 import { SignIn } from '../Screens/SignIn';
+
 
 const { Navigator, Screen } = createStackNavigator();
 
 export function AuthRoutes() {
+    const theme = useTheme();
+
   return(
-    <Navigator>
+    <Navigator
+    screenOptions={{
+      headerShown: false,
+    }}
+    >
       <Screen 
         name="SignIn"
         component={SignIn}
